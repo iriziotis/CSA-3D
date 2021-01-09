b31c7a903cbc027ac6a39e1f1f926875b9230470
Ioannis Riziotis <riziotis@ebi.ac.uk>
2021-01-09 15:43:09 +0000

    Fixed bugs in setting residue structure

--- tests/pickle_test.py
+++ tests/pickle_test.py
@@ -3,10 +3,15 @@ import sys
 sys.path.append('/Users/riziotis/ebi/phd/src/')
 import pickle
 
+print('Loading entry')
 with open('entry_1.ent', 'rb') as f:
     entry = pickle.load(f)
 
-#entry.pdbsites[1].write_pdb(outdir='./out')
+print('Fitting')
 for pdbsite in entry.pdbsites:
-    for i in pdbsite.mapped_unisites:
-        print(pdbsite.id, i.id)
+    if pdbsite.is_reference:
+        pdbsite.write_pdb(outdir='./out', write_hets=True)
+        continue
+    pdbsite.reference_site.fit(pdbsite, transform=True)
+    pdbsite.write_pdb(outdir='./out', write_hets=True)
+
--- tests/test.py
+++ tests/test.py
@@ -1,30 +1,31 @@
 #!/usr/bin/env python3
 
+import pickle
 import sys
 sys.path.append('/Users/riziotis/ebi/phd/src/')
 from mcsa3d import Mcsa
-import pickle
+
+entry = 1
 
 print('Initializing database')
 db = Mcsa()
 print('Building')
-db.build([1], annotate=False, verbose=True)
+db.build([entry], annotate=False, verbose=True)
 
-#print('Fitting')
-#for site in db.entries[1].pdbsites:
-#    if site.is_reference:
-#        site.write_pdb(outdir='./test', write_hets=True)
-#        continue
-#    try:
-#        site.reference_site.fit(site, transform=True, reorder=False, mutate=False, cycles=5)
-#    except Exception as e:
-#        print(site.id, e)
-#        continue
-#    site.write_pdb(outdir='./test', write_hets=True)
+print('Fitting')
+for site in db.entries[entry].pdbsites:
+    if site.is_reference:
+        site.write_pdb(outdir='./out', write_hets=True)
+        continue
+    try:
+        site.reference_site.fit(site, transform=True, reorder=False, mutate=False, cycles=5)
+    except Exception as e:
+        continue
+    site.write_pdb(outdir='./out', write_hets=True)
 
-print('Serializing')
-with open('entry_4.ent', 'wb') as o:
-    pickle.dump(db.entries[1], o)
+#print('Serializing')
+#with open('entry_{}.ent'.format(entry), 'wb') as o:
+#    pickle.dump(db.entries[entry], o)
 
 
 
