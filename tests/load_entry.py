33d0853b4294bd863f5aab04f9384de450aadd9a
Ioannis Riziotis <riziotis@ebi.ac.uk>
2021-01-11 10:18:50 +0000

    Added UniProt sequence mapping to PDB sites

--- tests/load_entry.py
+++ tests/load_entry.py
@@ -9,9 +9,10 @@ def main(mcsa_id):
         entry = pickle.load(f)
 
     for pdbsite in entry.pdbsites:
-        rot, tran, rms, rms_all = pdbsite.reference_site.fit(pdbsite, transform=True)
-        print(pdbsite.id, rms, rms_all)
-        pdbsite.write_pdb(outdir='./out', write_hets=True)
+        print(pdbsite.id, pdbsite.uniprot_id, ','.join(i.id for i in pdbsite.mapped_unisites))
+        #rot, tran, rms, rms_all = pdbsite.reference_site.fit(pdbsite, transform=True)
+        #print(pdbsite.id, rms, rms_all)
+        #pdbsite.write_pdb(outdir='./out', write_hets=True)
 
 if __name__ == '__main__':
     main(int(sys.argv[1]))
